20b1113bf223273cda31a14a82c9d573a342de4a
Damien Nguyen <ngn.damien@gmail.com>
2021-06-17 15:40:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,134 @@
+# pre-commit hooks
+
+This is a [pre-commit](https://pre-commit.com) hooks repo that integrates C/C++ linters/formatters to work with CMake-based projects.
+> [clang-format](https://clang.llvm.org/docs/ClangFormatStyleOptions.html),
+[clang-tidy](https://clang.llvm.org/extra/clang-tidy/),
+[cppcheck](http://cppcheck.sourceforge.net/) and
+[iwyu](https://include-what-you-use.org/)
+
+It is largely based on the work found [here](https://github.com/pocc/pre-commit-hooks). The main difference with POCC's
+pre-commit hooks is that the ones from this repository will do a CMake configuration step prior to running any
+pre-commit hooks. This is done in order to have CMake generate the compilation database file that can then be used by
+the various hooks (using the `-DCMAKE_EXPORT_COMPILE_COMMANDS=ON` CMake option).
+
+This repository is only has Python-based pre-commit hooks.
+
+
+## Example usage
+
+Assuming that you have the following directory structure for your projects
+
+    root
+    ├── .pre-commit-config.yaml
+    ├── CMakeLists.txt
+    └── src
+        └── err.cpp
+
+with the following file contents:
+
+__.pre-commit-config.yaml__
+
+    repos:
+      - repo: /Users/damien/code/pre-commit/cmake-pre-commit-hooks
+        rev: 1.0.0
+        hooks:
+          - id: clang-format
+          - id: clang-tidy
+            args: [--checks=readability-magic-numbers,--warnings-as-errors=*]
+          - id: cppcheck
+
+
+__CMakeLists.txt__
+
+    cmake_minimum_required(VERSION 3.15)
+    project(LANGUAGE CXX)
+    add_library(mylib STATIC src/err.cpp)
+
+__src/err.cpp__
+
+    #include <string>
+    int main() { int i; return 10; }
+
+Runnning pre-commit on the above project will lead to an output similar to this one:
+
+    $ pre-commit run --all-files
+    clang-format.............................................................Failed
+    - hook id: clang-format
+    - exit code: 1
+
+    src/err.cpp
+    ====================
+    <  int main() { int i; return 10; }
+    ---
+    >  int main() {
+    >    int i;
+    >    return 10;
+    >  }
+
+    clang-tidy...............................................................Failed
+    - hook id: clang-tidy
+    - exit code: 1
+
+    /tmp/temp/src/err.cpp:2:28: error: 10 is a magic number; consider replacing it with a named constant [readability-magic-numbers,-warnings-as-errors]
+    int main() { int i; return 10; }
+                               ^
+
+    cppcheck.................................................................Failed
+    - hook id: cppcheck
+    - exit code: 1
+
+    /tmp/temp/src/err.cpp:2:18: style: Unused variable: i [unusedVariable]
+    int main() { int i; return 10; }
+                     ^
+
+Note that your mileage may vary depending on the version of the tools. The example above was generated using `clang-format` 12.0.0, `clang-tidy` 12.0.0 and `cppcheck` 2.4.1.
+
+## Using the Hooks
+
+Python 3.6+ is required to use these hooks as all 5 invoking scripts are written in it. As this is also the minimum
+version of pre-commit, this should not be an issue.
+
+Running multiple hooks in parallel is currently supported by using the `fastener` Python package. If the hooks are run in parallel, only one of the hooks will run the CMake configure step while the others will simply wait until the call to CMake ends to continue. In the case where the hooks are run serially, all the hooks will be running the CMake configure step. However, if nothing changed in your CMake configuration, this should not cost too much time.
+
+
+### Installation
+
+For installing the various utilities, refer to your package manager documentation. Some guidance can also be found
+[here](https://github.com/pocc/pre-commit-hooks#installation).
+
+
+### Hook Info
+
+| Hook Info                                                                | Type                 | Languages                             |
+| ------------------------------------------------------------------------ | -------------------- | ------------------------------------- |
+| [clang-format](https://clang.llvm.org/docs/ClangFormatStyleOptions.html) | Formatter            | C, C++, ObjC                          |
+| [clang-tidy](https://clang.llvm.org/extra/clang-tidy/)                   | Static code analyzer | C, C++, ObjC                          |
+| [cppcheck](http://cppcheck.sourceforge.net/)                             | Static code analyzer | C, C++                                |
+
+
+### Hook Option Comparison
+
+| Hook Options                                                             | Fix In Place | Enable all Checks                             | Set key/value |
+| ------------------------------------------------------------------------ | ------------ | --------------------------------------------- | --------------- |
+| [clang-format](https://clang.llvm.org/docs/ClangFormatStyleOptions.html) | `-i`         |                   | |
+| [clang-tidy](https://clang.llvm.org/extra/clang-tidy/)                   | `--fix-errors` [1] | `-checks=*` `-warnings-as-errors=*` [2] | |
+| [cppcheck](http://cppcheck.sourceforge.net/)                             |  | `-enable=all` | |
+
+[1]: `-fix` will fail if there are compiler errors. `-fix-errors` will `-fix` and fix compiler errors if it can, like missing semicolons.
+
+[2]: Be careful with `-checks=*`.  can have self-contradictory rules in newer versions of llvm (9+): modernize wants to use [trailing return type](https://clang.llvm.org/extra/clang-tidy/checks/modernize-use-trailing-return-type.html) but Fuchsia [disallows it](https://clang.llvm.org/extra/clang-tidy/checks/fuchsia-trailing-return.html).
+
+
+### The '--' doubledash option
+
+Options after `--` like `-std=c++11` will be interpreted correctly for `clang-tidy`. Make sure they sequentially follow
+the `--` argument in the hook's args list.
+
+
+### Standalone Hooks
+
+If you want to have predictable return codes for your C linters outside of pre-commit, these hooks are available via
+[PyPI](https://pypi.org/project/CLinters/).  Install it with `pip install CLinters`.  They are named as `cmake-pc-$cmd-hook`, so
+`clang-format` becomes `cmake-pc-clang-format-hook`.
+
+If you want to run the tests below, you will need to install them from PyPI or locally with `pip install .`.
---
+++ cmake_pc_hooks/_utils.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Damien Nguyen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""""Base classes and functions for C/C++ linters and formatters"""
+
+import argparse
+import os
+import subprocess as sp
+from pathlib import Path
+import shutil
+import sys
+
+import fasteners
+import hooks.utils
+
+
+def get_cmake_command(cmake_names=None):
+    """
+    Get the path to a CMake executable on the PATH or in the virtual environment
+
+    Args:
+        cmake_names (:obj:`list` of :obj:`str`): Names for the CMake command
+            Defaults to ['cmake', 'cmake3']
+    """
+    if not cmake_names:
+        cmake_names = ['cmake', 'cmake3']
+
+    for cmake in cmake_names:
+        with open(os.devnull, 'w') as devnull:
+            try:
+                sp.check_call([cmake, '--version'], stdout=devnull, stderr=devnull)
+                return [shutil.which(cmake)]
+            except (OSError, sp.CalledProcessError):
+                pass
+
+            # CMake not in PATH, should have installed Python CMake module
+            # -> try to find out where it is
+            try:
+                root_path = os.environ['VIRTUAL_ENV']
+                python = os.path.basename(sys.executable)
+            except KeyError:
+                root_path, python = os.path.split(sys.executable)
+
+            search_paths = [root_path, os.path.join(root_path, 'bin'), os.path.join(root_path, 'Scripts')]
+
+            # First try executing CMake directly
+            for base_path in search_paths:
+                try:
+                    cmake_cmd = os.path.join(base_path, cmake)
+                    sp.check_call([cmake_cmd, '--version'], stdout=devnull, stderr=devnull)
+                    return [cmake_cmd]
+                except (OSError, sp.CalledProcessError):
+                    pass
+
+            # That did not work: try calling it through Python
+            for base_path in search_paths:
+                try:
+                    cmake_cmd = [python, os.path.join(base_path, 'cmake')]
+                    sp.check_call(cmake_cmd + ['--version'], stdout=devnull, stderr=devnull)
+                    return [cmake_cmd]
+                except (OSError, sp.CalledProcessError):
+                    pass
+
+    # Nothing worked -> give up!
+    return None
+
+
+def executable_path(path):
+    """
+    Argparse validation function
+
+    Args:
+        path (str): Path to some file or directory
+
+    Returns:
+        True if `path` points to a file that is executable, False otherwise
+    """
+    if os.path.isfile(path) and os.access(path, os.X_OK):
+        return path
+    raise argparse.ArgumentTypeError(f'{path} is not a valid file and/or does not appear executable')
+
+
+class Command(hooks.utils.Command):  # pylint: disable=R0902
+    """
+    Super class that all commands inherit.
+    """
+
+    def __init__(self, command, look_behind, args):
+        super().__init__(command, look_behind, args)
+        self.cmake = get_cmake_command()
+        self.clean_build = False
+        self.source_dir = '..'
+        self.build_dir = '.cmake_build'
+        self.cmake_args = ['-DCMAKE_EXPORT_COMPILE_COMMANDS=ON']
+        self.parse_args(args)
+
+    def parse_args(self, args):
+        """
+        Parse some arguments into some usable variables
+
+        Args:
+            args (:obj:`list` of :obj:`str`): list of arguments
+        """
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-S', '--source-dir', type=str, help='Path to build directory', default=self.source_dir)
+        parser.add_argument('-B', '--build-dir', type=str, help='Path to build directory', default=self.build_dir)
+        parser.add_argument('-D', dest='defines', nargs='*', type=str, help='Create or update a cmake cache entry.')
+        parser.add_argument(
+            '-U', dest='undefines', nargs='*', type=str, help='Remove matching entries from CMake cache.'
+        )
+        parser.add_argument('-G', dest='generator', type=str, help='Specify a build system generator.')
+        parser.add_argument('-T', dest='toolset', type=str, help='Specify toolset name if supported by generator.')
+        parser.add_argument('-A', dest='platform', type=str, help='Specify platform if supported by generator.')
+        parser.add_argument('-Werror', dest='errors', nargs='*', type=str, help='Make developer warnings errors.')
+        parser.add_argument(
+            '-Wno-error', dest='no_error', nargs='*', type=str, help='Make developer warnings not errors.'
+        )
+        parser.add_argument(
+            '--cmake', type=executable_path, help='Make developer warnings not errors.', default=self.cmake
+        )
+
+        parser.add_argument('-Wdev', action='store_true', help='Enable developer warnings.')
+        parser.add_argument('-Wno-dev', action='store_true', help='Suppress developer warnings.')
+
+        if not self.clean_build:
+            parser.add_argument('--clean', action='store_true', help='Start from a clean build directory')
+        known_args, args = parser.parse_known_args(args)
+        self.clean_build = known_args.clean
+        self.source_dir = Path(known_args.source_dir)
+        self.build_dir = Path(known_args.build_dir)
+        self._setup_cmake_args(known_args)
+
+        if not self.source_dir.exists() and not self.source_dir.is_dir():
+            sys.stderr.write(f'{self.source_dir} is not a valid source directory\n')
+            sys.exit(1)
+
+        super().parse_args(args)
+
+    def run_command(self, filename):
+        """Run the command and check for errors"""
+
+        # Always try to configure using CMake before any other commands
+        self._run_cmake_configure()
+
+        super().run_command(filename)
+
+    def _run_cmake_configure(self):  # pylint: disable=R0912
+        configuring = Path(self.build_dir, '_configuring')
+        has_lock = False
+
+        self.build_dir.mkdir(exist_ok=True)
+
+        rw_lock = fasteners.InterProcessReaderWriterLock(configuring)
+        if not configuring.exists():
+            with rw_lock.write_lock():
+                has_lock = True
+                if self.clean_build:
+                    for path in self.build_dir.iterdir():
+                        if path.is_dir():
+                            shutil.rmtree(path)
+                        elif path != configuring:
+                            path.unlink()
+                try:
+                    sp.run(
+                        self.cmake + [self.source_dir] + self.cmake_args,
+                        cwd=self.build_dir,
+                        check=True,
+                        stdout=sp.PIPE,
+                        stderr=sp.PIPE,
+                    )
+                except sp.CalledProcessError as e:
+                    self.stdout = f'Running CMake with: {self.cmake + self.cmake_args}' + e.output.decode()
+                    self.stderr = e.stderr.decode()
+                    self.returncode = e.returncode
+                else:
+                    self.returncode = 0
+
+                compiledb = Path(self.build_dir, 'compile_commands.json')
+                if self.returncode == 0:
+                    if compiledb.exists():
+                        shutil.copy(compiledb, self.source_dir)
+                    else:
+                        self.returncode = 1
+                        self.stderr += f'Unable to locate {compiledb}\n\n'
+
+                if self.returncode != 0:
+                    sys.stdout.write(self.stdout + '\n')
+                    sys.stderr.write(self.stderr)
+        else:
+            with rw_lock.read_lock():
+                pass
+
+        if has_lock and configuring.exists():
+            configuring.unlink()
+
+        if self.returncode != 0:
+            sys.exit(self.returncode)
+
+    def _setup_cmake_args(self, args):
+        self.cmake = args.cmake if isinstance(args.cmake, list) else [args.cmake]
+        if not self.cmake:
+            raise RuntimeError('Unable to locate CMake command')
+
+        if args.defines:
+            for define in args.defines:
+                self.cmake_args.append(f'-D{define}')
+        if args.undefines:
+            for undefine in args.undefines:
+                self.cmake_args.append(f'-U{undefine}')
+        if args.errors:
+            for error in args.errors:
+                self.cmake_args.append(f'-Werror={error}')
+
+        if args.generator:
+            self.cmake_args.append(f'-G{args.generator}')
+        if args.toolset:
+            self.cmake_args.append(f'-T{args.toolset}')
+        if args.platform:
+            self.cmake_args.append(f'-A{args.platform}')
+
+        if args.Wdev:
+            self.cmake_args.append('-Wdev')
+        if args.Wno_dev:
+            self.cmake_args.append('-Wno-dev')
+
+
+class ClangAnalyzerCmd(Command, hooks.utils.ClangAnalyzerCmd):
+    """Commands that statically analyze code: clang-tidy, oclint"""
+
+
+class FormatterCmd(Command, hooks.utils.FormatterCmd):
+    """Commands that format code: clang-format, uncrustify"""
