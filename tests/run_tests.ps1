587a2e6f12b6f369c9abb5664f4180fe69240c85
Damien Nguyen <ngn.damien@gmail.com>
2021-06-22 15:14:27 +0200

    Fix build on Windows for CI

--- tests/run_tests.ps1
+++ tests/run_tests.ps1
@@ -1,17 +1,22 @@
 $build_dir = "build"
+$CMakeArgs = '-GNinja', '-DCMAKE_C_COMPILER="cl.exe"', '-DCMAKE_CXX_COMPILER="cl.exe"'
 
 Set-PSDebug -trace 1
 
 Function run_hook
 {
-    Param($Hook, $Src, $ExpectSuccess, $GenCompileDB)
+    Param($Hook,
+          $Src,
+          $ExpectSuccess,
+          $GenCompileDB)
+
 
     mkdir -Path $build_dir -Force
     rm -Recurse -Force $build_dir/*
 
     if ($GenCompileDB -eq 1) {
-        Write-Output "$hook -S $Src -B $build_dir -G Ninja $Args"
-        & $hook -S $Src -B $build_dir -G Ninja @Args
+        Write-Output "$hook -S $Src -B $build_dir $CMakeArgs $Args"
+        & $hook -S $Src -B $build_dir @CMakeArgs @Args
     }
     else {
         Write-Output "$hook $Args"
@@ -33,7 +38,7 @@ Function run_hook
     }
 
     if ($GenCompileDB -eq 1) {
-        if (Test-Path -Path $build_dir/compile_commands.json -PathType leaf) {
+        if (-not (Test-Path -Path $build_dir/compile_commands.json -PathType Leaf)) {
             echo "Hook $hook failed to generate the compile database for $Src!"
             Exit 1
         }
@@ -41,10 +46,21 @@ Function run_hook
 }
 
 run_hook -Hook cmake-pc-clang-format-hook -Src tests/cmake_good -ExpectSuccess 1 -GenCompileDB 0 tests/cmake_good/good.cpp
-# run_hook -Hook cmake-pc-clang-tidy-hook -Src tests/cmake_good -ExpectSuccess 1 -GenCompileDB 1 --checks=readability-magic-numbers --warnings-as-errors=* tests/cmake_good/good.cpp
-# run_hook -Hook cmake-pc-cppcheck-hook -Src tests/cmake_good -ExpectSuccess 1 -GenCompileDB 1 tests/cmake_good/good.cpp
+run_hook -Hook cmake-pc-clang-tidy-hook -Src tests/cmake_good -ExpectSuccess 1 -GenCompileDB 1 --checks=readability-magic-numbers --warnings-as-errors=* tests/cmake_good/good.cpp
+run_hook -Hook cmake-pc-cppcheck-hook -Src tests/cmake_good -ExpectSuccess 1 -GenCompileDB 1 tests/cmake_good/good.cpp
+
+mkdir -Path $build_dir -Force
+rm -Recurse -Force $build_dir/*
+cmake -B other_build -S tests/cmake_good @CMakeArgs
+cmake-pc-cppcheck-hook -S tests/cmake_good -B build -B other_build @CMakeArgs tests/cmake_good/good.cpp
+if (-not (Test-Path -Path other_build/compile_commands.json -PathType Leaf)) {
+    echo "Failed to generate the compile database for other_build!"
+    Exit 1
+}
+
+
 run_hook -Hook cmake-pc-clang-format-hook -Src tests/cmake_bad -ExpectSuccess 0 -GenCompileDB 0 tests/cmake_bad/bad.cpp
-# run_hook -Hook cmake-pc-clang-tidy-hook -Src tests/cmake_bad -ExpectSuccess 0 -GenCompileDB 1 --checks=readability-magic-numbers --warnings-as-errors=* tests/cmake_bad/bad.cpp
-# run_hook -Hook cmake-pc-cppcheck-hook -Src tests/cmake_bad -ExpectSuccess 0 -GenCompileDB 1 tests/cmake_bad/bad.cpp
+run_hook -Hook cmake-pc-clang-tidy-hook -Src tests/cmake_bad -ExpectSuccess 0 -GenCompileDB 1 --checks=readability-magic-numbers --warnings-as-errors=* tests/cmake_bad/bad.cpp
+run_hook -Hook cmake-pc-cppcheck-hook -Src tests/cmake_bad -ExpectSuccess 0 -GenCompileDB 1 tests/cmake_bad/bad.cpp
 
 Exit 0
