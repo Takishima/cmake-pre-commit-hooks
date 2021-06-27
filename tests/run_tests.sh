0dd350f32ff52246ea1c9e7b332bddf1c44c2541
Nguyen Damien <ngn.damien@gmail.com>
2021-06-27 19:11:27 +0200

    Enable debug output for Include-What-You-Use during testing for Unix

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -47,7 +47,7 @@ function run_hook()
 run_hook cmake-pc-clang-format-hook tests/cmake_good 1 0 tests/cmake_good/good.cpp
 run_hook cmake-pc-clang-tidy-hook tests/cmake_good 1 1 --checks=readability-magic-numbers --warnings-as-errors=* tests/cmake_good/good.cpp
 run_hook cmake-pc-cppcheck-hook tests/cmake_good 1 1 tests/cmake_good/good.cpp
-run_hook cmake-pc-include-what-you-use-hook tests/cmake_good 1 1 tests/cmake_good/good.cpp
+run_hook cmake-pc-include-what-you-use-hook tests/cmake_good 1 1 --debug tests/cmake_good/good.cpp
 
 # Test multiple build directory options
 mkdir -p $build_dir
@@ -64,7 +64,7 @@ fi
 run_hook cmake-pc-clang-format-hook tests/cmake_bad 0 0 tests/cmake_bad/bad.cpp
 run_hook cmake-pc-clang-tidy-hook tests/cmake_bad 0 1 --checks=readability-magic-numbers --warnings-as-errors=* tests/cmake_bad/bad.cpp
 run_hook cmake-pc-cppcheck-hook tests/cmake_bad 0 1 tests/cmake_bad/bad.cpp
-run_hook cmake-pc-include-what-you-use-hook tests/cmake_bad 0 1 tests/cmake_bad/bad.cpp
+run_hook cmake-pc-include-what-you-use-hook tests/cmake_bad 0 1 --debug tests/cmake_bad/bad.cpp
 
 # ==============================================================================
 
